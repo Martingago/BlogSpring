b8667a6f42bda136f26cdf3e626a76cf5cd5a2b0
Martin <gagochorenmartin@gmail.com>
2024-09-03 14:13:30 +0200

    .

--- src/main/java/com/project/springBlog/SpringBlogApplication.java
+++ src/main/java/com/project/springBlog/SpringBlogApplication.java
@@ -1,7 +1,5 @@
 package com.project.springBlog;
 
-import io.github.cdimascio.dotenv.Dotenv;
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
