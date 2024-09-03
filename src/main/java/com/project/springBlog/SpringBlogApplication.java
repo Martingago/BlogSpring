4c2c450a906d48ae514de1c0f45e8901407cac28
Martin <gagochorenmartin@gmail.com>
2024-09-03 12:06:11 +0200

    + configuracion dotenv para Variables entorno

--- src/main/java/com/project/springBlog/SpringBlogApplication.java
+++ src/main/java/com/project/springBlog/SpringBlogApplication.java
@@ -1,12 +1,21 @@
 package com.project.springBlog;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class SpringBlogApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+
+		// Configura y carga las variables de entorno
+		System.setProperty("spring.datasource.url", dotenv.get("DB_URL"));
+		System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
+		System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(SpringBlogApplication.class, args);
 	}
 
