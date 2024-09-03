3f5fb3a3134470c426faddb9bdaef2e823892944
Martin <gagochorenmartin@gmail.com>
2024-09-03 13:29:56 +0200

    precarga variables entorno build proyecto

--- src/main/java/com/project/springBlog/SpringBlogApplication.java
+++ src/main/java/com/project/springBlog/SpringBlogApplication.java
@@ -1,22 +1,27 @@
 package com.project.springBlog;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 
 @SpringBootApplication
 public class SpringBlogApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
+		Dotenv dotenv = Dotenv.configure().directory("./").load();
 
 		// Configura y carga las variables de entorno
-		System.setProperty("spring.datasource.url", dotenv.get("DB_URL"));
-		System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
-		System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 
 		SpringApplication.run(SpringBlogApplication.class, args);
+	}
+
+	// Este método se asegura de que las variables se carguen antes de cualquier inicialización
+	@PostConstruct
+	public void init() {
+		Dotenv dotenv = Dotenv.configure().directory("./").load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 	}
 
 }
