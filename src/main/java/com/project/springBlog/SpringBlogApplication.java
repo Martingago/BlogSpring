938f34d265a05c70c16d08c1be0d7afa754d9d2a
Martin <gagochorenmartin@gmail.com>
2024-09-03 14:09:56 +0200

    .

--- src/main/java/com/project/springBlog/SpringBlogApplication.java
+++ src/main/java/com/project/springBlog/SpringBlogApplication.java
@@ -9,11 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBlogApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().directory("./").load();
-
-		// Configura y carga las variables de entorno
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
-
 		SpringApplication.run(SpringBlogApplication.class, args);
 	}
 }
